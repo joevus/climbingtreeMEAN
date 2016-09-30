f19d461396232df2778bec37a07bb786427921f7
Joe Hos <joehoskisson@gmail.com>
2016-09-30 15:47:35 -0600

    Added pseudo code to DrawingService, played with css for .subtopic class

--- public/components/topics/drawingService.js
+++ public/components/topics/drawingService.js
@@ -3,6 +3,12 @@ var app = angular.module("TreeApp");
 
 app.service("DrawingService", [function(){
     
+    // arrange subtopics in two columns in topics.html
     
+    // add canvas html element to topics.html
+    
+    // get the positions of the main topic and subtopics
+    
+    // use positions to draw lines on canvas
     
 }]);
