2128a5466e5b061edb6feab5e9470a4b59918c4d
Joe Hos <joehoskisson@gmail.com>
2016-10-04 11:13:27 -0600

    reorganized and cleaned up code

---
+++ public/components/topics/drawing.js
@@ -0,0 +1,81 @@
+// For drawing lines to connect the topics like a pedigree
+var app = angular.module("TreeApp");
+
+app.service("DrawingService", [function () {
+
+        this.y2List = [];
+
+        this.clearCanvas = function () {
+            console.log("in clear canvas");
+            var canvas = document.getElementById("topicCanvas");
+            var ctx = canvas.getContext('2d');
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+        }
+
+        this.drawLines = function (x1, x2, x3, y1, y2, y3) {
+            // draw lines between main topic and subtopics
+
+            var canvas = document.getElementById("topicCanvas");
+            var ctx = canvas.getContext('2d');
+            ctx.beginPath();
+            ctx.moveTo(x1, y1);
+            ctx.lineTo(x2, y2);
+            ctx.lineTo(x3, y3);
+            ctx.stroke();
+
+        }
+
+
+}])
+    .directive('drawingDirective', ["DrawingService", function (DrawingService) {
+        return function (scope, element, attrs) {
+
+            /*
+            determine left and right positions for all lines
+                - width of canvas
+                - use percentages, multiply total width
+            */
+            var canvas = document.getElementById("topicCanvas");
+
+            if (scope.$index % 2 === 0) {
+                var x1 = canvas.width * .04;
+                var x2 = canvas.width * .04;
+                var x3 = canvas.width * .10;
+            } else {
+                var x1 = canvas.width * .49;
+                var x2 = canvas.width * .49;
+                var x3 = canvas.width * .55;
+            }
+
+
+            /*
+            - determine vertical starting point for buttons 1 and 2
+                - top = 0
+            - vertical ending point for all buttons
+                - (y position of button) + 1/2 * (height of button)
+            - vertical starting point for buttons 3 and beyond
+                - lowest vertical point for nth-2 button
+                - found with y2List[scope.$index-2]
+                
+            */
+
+            var mainTopic = document.getElementsByClassName("main-topic");
+            var mainTopicHeight = mainTopic[0].getBoundingClientRect().height;
+            var buttonTop = element[0].getBoundingClientRect().top - mainTopicHeight;
+            var buttonHeight = element[0].getBoundingClientRect().height;
+
+            if (scope.$index <= 1) {
+                var y1 = 0;
+            } else {
+                var y1 = DrawingService.y2List[scope.$index - 2];
+            }
+
+            var y2 = buttonTop + .5 * buttonHeight;
+            var y3 = buttonTop + .5 * buttonHeight;
+            DrawingService.y2List.push(y2);
+
+            // use positions to draw lines on canvas
+            DrawingService.drawLines(x1, x2, x3, y1, y2, y3);
+
+        }
+}]);
--- public/components/topics/topics.js
+++ public/components/topics/topics.js
@@ -2,8 +2,8 @@ var app = angular.module("TreeApp");
 
 app.controller("TopicsController", ["$scope", "TopicService", "DrawingService", "$location", function($scope, TopicService, DrawingService, $location){
     
+    // store canvas width to use in topics.html
     var canvasContainer = document.getElementsByClassName("canvasContainer")[0];
-    
     $scope.canvasWidth = canvasContainer.getBoundingClientRect().width;
     
     $scope.TopicService = TopicService;
@@ -11,48 +11,4 @@ app.controller("TopicsController", ["$scope", "TopicService", "DrawingService", 
     // update topicList based on topic parent name
     TopicService.beginTopics();
     
-}])
-
-.directive('buttonDirective', ["DrawingService", function(DrawingService) {
-    return function(scope, element, attrs) {
-        console.log(element);
-        var mainTopic = document.getElementsByClassName("main-topic");
-        var mainTopicHeight = mainTopic[0].getBoundingClientRect().height;
-        var buttonTop = element[0].getBoundingClientRect().top - mainTopicHeight;
-        var buttonHeight = element[0].getBoundingClientRect().height;
-        var canvas = document.getElementById("topicCanvas");
-        
-        if (scope.$index % 2 === 0) {
-                var x1 = canvas.width * .04;
-                var x2 = canvas.width * .04;
-                var x3 = canvas.width * .10;
-        } else {
-            var x1 = canvas.width * .49;
-            var x2 = canvas.width * .49;
-            var x3 = canvas.width * .55;
-        }
-
-        if (scope.$index <= 1) {
-            var y1 = 0;
-        } else {
-            var y1 = DrawingService.y2List[scope.$index - 2];
-        }
-        
-        var y2 = buttonTop + .5 * buttonHeight;
-        var y3 = buttonTop + .5 * buttonHeight;
-        DrawingService.y2List.push(y2);
-        
-        console.log(buttonTop);
-        console.log("inside directive, x1: " + x1);
-        console.log("inside directive, y1: " + y1);
-        console.log("inside directive, x2: " + x2);
-        console.log("inside directive, y2: " + y2);
-        
-        var ctx = canvas.getContext('2d');
-        ctx.beginPath();
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(x2, y2);
-        ctx.lineTo(x3, y3);
-        ctx.stroke();
-    }
 }]);
