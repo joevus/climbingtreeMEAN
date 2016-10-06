8436b82e37048b0089b32dc39fdc1e7eb2b78ce2
Joe Hos <joehoskisson@gmail.com>
2016-10-05 18:19:24 -0600

    Put in a demo bootstrap-ui carousel

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("TreeApp", ["ngRoute"]);
+var app = angular.module("TreeApp", ["ngRoute", "ui.bootstrap"]);
 
 app.config(["$routeProvider", function ($routeProvider) {
     $routeProvider
