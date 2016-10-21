af79a36bca6338d60a50e265ec1f358701588b38
Joe Hos <joehoskisson@gmail.com>
2016-10-21 13:32:30 -0600

    Added AuthInterceptor factory and pushed it into interceptors

--- public/components/auth/auth.js
+++ public/components/auth/auth.js
@@ -54,3 +54,27 @@ app.service("UserService", ["$http", "$location", "TokenService", function($http
         return !!TokenService.getToken();
     };
 }]);
+
+app.factory("AuthInterceptor", ["$q", "$location", "TokenService", function($q, $location, TokenService) {
+    return {
+        request: function (config) {
+            var token = TokenService.getToken();
+            if(token) {
+                config.headers = config.headers || {};
+                config.headers.Authorization = "Bearer " + token;
+            }
+            return config;
+        },
+        responseError: function(response) {
+            if (response.status === 401) {
+                TokenService.removeToken();
+                $location.path("/login");
+            }
+            return $q.reject(response);
+        }
+    }
+}]);
+
+app.config(function ($httpProvider) {
+    $httpProvider.interceptors.push('AuthInterceptor');
+});
