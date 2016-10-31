12360bae1bd368917a320a1689e1c1693530f795
Joe Hos <joehoskisson@gmail.com>
2016-10-31 11:03:33 -0600

    Changed controller of tile-editor directive to correct controller. Topics now showing up in edit mode.

--- public/components/tile/tile-editor.js
+++ public/components/tile/tile-editor.js
@@ -1,6 +1,6 @@
 var app = angular.module("TreeApp");
 
-app.controller("TileEditorCtrl", ["$scope", "ResourceService", "$location", function ($scope, ResourceService, $location) {
+app.controller("TileEditorCtrl", ["$scope", "ResourceService", "$location", "TopicService", function ($scope, ResourceService, $location, TopicService) {
 
     $scope.ResourceService = ResourceService;
     $scope.goToResourcePage = function(resource) {
@@ -10,12 +10,17 @@ app.controller("TileEditorCtrl", ["$scope", "ResourceService", "$location", func
     // function decides whether to return the star or yellowStar
     $scope.determineStar = ResourceService.determineStar;
     
+    console.log("hello");
+    TopicService.getAllTopics().then(function(response) {
+        $scope.allTopics = response.data; 
+    });
+    
 }])
     .directive("tileEditor", function () {
         return {
             restrict: 'AE',
             templateUrl: "components/tile/tile-editor.html",
             replace: true,
-            controller: "TileCtrl"
+            controller: "TileEditorCtrl"
         }
     })
