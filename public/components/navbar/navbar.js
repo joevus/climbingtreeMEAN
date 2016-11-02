003adfcd237c4341473f71016ed2cb29d869224f
Joe Hos <joehoskisson@gmail.com>
2016-11-02 12:59:31 -0600

    Made navbar active class switch depending on route. Added controller in navbar.js to do this.

--- public/components/navbar/navbar.js
+++ public/components/navbar/navbar.js
@@ -1,10 +1,17 @@
 var app = angular.module("TreeApp");
 
+app.controller("NavbarController", ["$scope", "$location", function($scope, $location) {
+    $scope.isActive = function(viewLocation) {
+        return viewLocation === $location.path();
+    };
+}]);
+
 app.directive("navbar", ["UserService", function(UserService) {
     return {
         templateUrl: "components/navbar/navbar.html",
         link: function(scope) {
             scope.userService = UserService;
-        }
+        },
+        controller: "NavbarController"
     }
 }]);
